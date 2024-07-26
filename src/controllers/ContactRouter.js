5cd791e861cd8baeba88e80b1bbe3cfbf6a89503
Calfura <travis_wilson999@hotmail.com>
2024-07-26 12:40:33 +1000

    feat: CRUD skeleton, get all Contacts implemeneted

--- src/controllers/ContactRouter.js
+++ src/controllers/ContactRouter.js
@@ -1,10 +1,44 @@
 const express = require("express");
+const { Contact } = require("../ultis/models/ContactModel");
 const router = express.Router();
 
 router.get("/", (request, response) => {
     response.json({
         message:"Contact router activated!"
     });
+});
+
+
+// localhost:3000/contacts/all
+router.get("/all", async(request, response) => {
+    let results = await Contact.find().exec();
+    console.log("Found documents!")
+    console.log(results)
+    response.json({
+        message: "Found documents!",
+        data: results
+    });
+});
+
+// GET localhost:3000/contacts/
+router.get("/", (request, response) =>{
+
+});
+
+// POST localhost:3000/contacts/
+router.post("/", (request, response) =>{
+
+});
+
+
+// PATCH localhost:3000/contacts/1234
+router.patch("/:id", (request, response) =>{
+
+});
+
+// DELETE localhost:3000/contacts/1234
+router.delete("/:id", (request, response) =>{
+
 });
 
 
